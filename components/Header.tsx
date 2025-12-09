85e3b4ea9f4d7b662e2956677254b793a20361ca
Nikhil Gupta <ng.nikhil674@gmail.com>
2025-12-09 22:57:57 +0530

    added container, header and footer

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Container from "./Container";
+
+const Header = () => {
+  return (
+    <header className="bg-white py-5">
+      <Containe>
+        {/* Logo */}
+        {/* NavButton */}
+        {/* NavAdmin */}
+      </Containe>
+    </header>
+  );
+};
+
+export default Header;
