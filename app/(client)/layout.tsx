f70fdb68aa0a728a2eb450d08c7c61054cc97d7f
Nikhil Gupta <ng.nikhil674@gmail.com>
2025-12-23 02:07:34 +0530

    corrected everything, implemented studio using sanity

---
+++ app/(client)/layout.tsx
@@ -0,0 +1,28 @@
+import "../globals.css";
+import { ReactNode } from "react";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - Shopcard Online store",
+    default: "Shopcart online store",
+  },
+  description: "Shopcart online store, Your one step for shop all your needs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <ClerkProvider>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </ClerkProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,13 @@
 import { Poppins } from "next/font/google";
-import "./globals.css";
-import { ReactNode } from "react";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
-import { Metadata } from "next";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "600"],
 });
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - Shopcard Online store",
-    default: "Shopcart online store",
-  },
-  description: "Shopcart online store, Your one step for shop all your needs",
-};
-
-export default function RootLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${poppins.className} antialiased`}>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${poppins.className} antialiased`}>{children}</body>
+    </html>
   );
-}
+};
+export default RootLayout;
