9271d551c32604b690bfb9dcfc77bcd79d6c3a8b
Nikhil Gupta <ng.nikhil674@gmail.com>
2025-12-23 01:31:39 +0530

    completed the footer and added it to the main app and updated the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,13 @@ import { ReactNode } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Metadata } from "next";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "600"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: {
     template: "%s - Shopcard Online store",
     default: "Shopcart online store",
@@ -17,14 +18,18 @@ export const metadata = {
   description: "Shopcart online store, Your one step for shop all your needs",
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={`${poppins.className} antialiased`}>
-          <Header />
-          {children}
-          <Footer />
+          <div className="flex flex-col min-h-screen">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
         </body>
       </html>
     </ClerkProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import Container from "@/components/Container";
+import HomeBanner from "@/components/HomeBanner";
 import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Home = () => {
   return (
-    <Container className=" bg-shop_light_pink">
-      <h2 className="text-xl font-semibold">Home</h2>
-      <p>Hello my name is nikhil</p>
-      <Button size="lg">Check it out</Button>
+    <Container>
+      <HomeBanner />
     </Container>
   );
 };
