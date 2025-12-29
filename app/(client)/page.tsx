bbb2622f195b0a00f27d130e90f740adda48e1d7
Nikhil Gupta <ng.nikhil674@gmail.com>
2025-12-30 03:24:43 +0530

    adding the categories page

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -2,16 +2,19 @@ import Container from "@/components/Container";
 import HomeBanner from "@/components/HomeBanner";
 import HomeCategories from "@/components/HomeCategories";
 import ProductGrid from "@/components/ProductGrid";
+import { getCategories } from "@/sanity/queries";
 import React from "react";
 
-const Home = () => {
+const Home = async () => {
+  const categories = await getCategories(6);
+
   return (
     <Container>
       <HomeBanner />
       <div className="py-10">
         <ProductGrid />
       </div>
-      <HomeCategories />
+      <HomeCategories categories={categories} />
     </Container>
   );
 };
