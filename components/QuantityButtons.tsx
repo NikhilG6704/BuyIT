8d177173f8aa0afcd33538916d936df5e3643292
Nikhil Gupta <ng.nikhil674@gmail.com>
2026-01-10 22:15:13 +0530

    Quantity button, add to cart button aal completed with inclusion of toast

--- components/QuantityButtons.tsx
+++ components/QuantityButtons.tsx
@@ -20,7 +20,9 @@ const QuantityButtons = ({ product, className }: Props) => {
     if (itemCount > 1) {
       toast.success("Quantity Decreased successfully!");
     } else {
-      toast.success(`${product?.name?.substring(0, 12)} removed successfully!`);
+      toast.success(
+        `${product?.name?.substring(0, 12)}... removed successfully!`
+      );
     }
   };
 
